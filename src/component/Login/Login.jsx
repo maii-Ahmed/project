2ee4ee9ffc4ada629ae5a7dd8c1c3f6c04db7262
mai ahmed <mai29200333@gmail.com>
2025-05-10 23:23:48 +0300

    update login

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -130,6 +130,25 @@ export default function Login() {
             required
           />
         </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            password
+          </label>
+          <input
+            name="password"
+            value={LoginFormik.values.password}
+            onChange={LoginFormik.handleChange}
+            onBlur={LoginFormik.handleBlur}
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="password"
+            required
+          />
+        </div>
         {LoginFormik.errors.password && LoginFormik.touched.password ? (
           <div
             className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
