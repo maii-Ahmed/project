34f151c411b026beb42e4a2e9080ccec8148c594
mai ahmed <mai29200333@gmail.com>
2025-05-10 23:41:08 +0300

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ import Addone from './component/Addone/Addone'
 import NotFound from './component/NotFound/NotFound'
 import ShowCourse from './component/ShowCourse/ShowCourse'
 import Home from './component/Home/Home'
-import Register from './component/Register/Register'
-import Login from './component/Login/Login'
+// import Register from './component/Register/Register'
+ import Login from './component/Login/Login'
 import ConttextProvider from './ContextProvider/ContextProvider'
 import Upload from './component/Upload/Upload'
 import AddAssistant from './component/AddAssistant/AddAssistant'
@@ -25,14 +25,14 @@ import AddAssistant from './component/AddAssistant/AddAssistant'
 export default function App() {
   let router=createBrowserRouter([
   {path:"",element:<Layout/>,children:[
-    {path:"",element:<Login/>},
+     {path:"",element:<Login/>},
     {path:"/Students",element:<Students/>},
     {path:"/Course",element:<Course/>},
     {path:"/Asisstants",element:<Asisstants/>},
     {path:"/Addone",element:<Addone/>},
     {path:"/showcourse",element:<ShowCourse/>},
     {path:"/Home",element:<Home/>},
-    {path:"/Register",element:<Register/>},
+    // {path:"/Register",element:<Register/>},
     {path:"/Attandance",element:<Attandance/>},
     {path:"/Upload",element:<Upload/>},
     {path:"/AddAssistant",element:<AddAssistant/>},
